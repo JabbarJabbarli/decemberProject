7822e6fbf2d90c614ea7b9f10f813056b91d7d18
JabbarJabbarli <cabbarcabbarli2005@gmail.com>
2024-12-17 00:46:47 +0400

    decemberProject beginning

---
+++ client/src/main.jsx
@@ -0,0 +1,8 @@
+import "./styles/global.css";
+import { router } from "./routes/index.jsx";
+import { createRoot } from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+
+createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);
---
+++ client/src/pages/home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1 className="text-yellow-500 font-extrabold">asdasd</h1>
+    </div>
+  );
+};
+
+export default HomePage;
